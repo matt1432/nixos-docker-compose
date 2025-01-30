ff73e5dc040b17eba21c7d02f51bb4759c710203
matt1432 <matt@nelim.org>
2025-01-30 09:20:26 -0500

    feat(module): add enable option

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,23 @@
-self: {...}: {
-  options = {};
+self: {
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf mkOption types;
 
-  config = {};
+  cfg = config.virtualisation.docker-compose;
+in {
+  options.virtualisation.docker-compose = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        This option enables docker-compose declaration in nix code.
+      '';
+    };
+  };
+
+  config = mkIf (cfg.enable) {};
 
   # For accurate stack trace
   _file = ./default.nix;
